47eac1512aed4d9ca046d220cb5f7d7048fa2351
Natalie Taktachev <ntaktach@gmail.com>
2019-07-22 20:25:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
-class Shoe 
+class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
 
-  def brand 
+  def brand
     @brand
   end
 end
